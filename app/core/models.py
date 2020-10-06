b9fcea61963bad8a060d28a335453d71ad788332
Marcin Węglowski <mawg@softwaremind.com>
2020-10-06 22:45:03 +0200

    Add __str__ metods on Character and CharacterRase

--- app/core/models.py
+++ app/core/models.py
@@ -4,12 +4,18 @@ from django.db import models
 class CharacterRace(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Character(models.Model):
     first_name = models.CharField(max_length=255)
-    middle_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
     race = models.ForeignKey(CharacterRace, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
 
 
 class Author(models.Model):
